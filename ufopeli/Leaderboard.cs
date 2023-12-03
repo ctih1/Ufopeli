1f5070639d21af60d188426be16fe0fdb8871fb1
ctih <78687256+ctih1@users.noreply.github.com>
2023-12-03 18:43:17 +0200

    Patch1 (#2)
    * Added a lot of things, moved everything back into the Ufopeli.cs class. TODO: particle systme. Aimassist is broken
    
    * Fixed a few things, leaderboard needs to be implemented
    
    * Changed the background
    
    ---------
    
    Co-authored-by: ctih <nevalaonni@proton.me>

---
+++ ufopeli/Leaderboard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.Http.Json;
+using System.Xml;
+using System.Net.Http;
+using System.Net;
+using System.Net.Mime;
+
+namespace ufopeli
+{
+	public class Leaderboard
+	{
+		HttpClient httpClient = new HttpClient();
+		HttpResponseMessage response;
+		HttpRequestMessage request;
+		public void StartServer()
+		{
+			
+			request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://ufopeliserver.ctih.repl.co/"),
+			};
+			response = httpClient.Send(request);
+
+		}
+		public Boolean SaveData(String user, int score)
+		{
+			// Settings TLS 1.2
+			System.Net.ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+			request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri(String.Format("https://ufopeliserver.ctih.repl.co/save?name={0}&score={1}",user,score)),
+			};
+			response = httpClient.Send(request);
+			return response.IsSuccessStatusCode;
+		} 
+
+	}
+}
--- ufopeli/Ufopeli.cs
+++ ufopeli/Ufopeli.cs
@@ -1,7 +1,15 @@
 using Jypeli;
 using Jypeli.Assets;
+using Jypeli.Effects;
+using Jypeli.Widgets;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+// TODO: Käännä "enemy.png" että se ei ole sivuttain.
 
 namespace ufopeli
 {
@@ -15,115 +23,353 @@ namespace ufopeli
 	// TODO: Healthbar
 	public class ufopeli : PhysicsGame
 	{
-		public int score = 0;
-		Label scoreText;
-		Label healthText;
-		private Jypeli.Shape healthBar;
-		int Width;
-		int Heigth;
+		// Display
+		readonly int WIDTH;
+		readonly int HEIGTH;
+		double scaling = 1.5;
+
+		// Font
 		int margin = 25;
 		int fontSize = 52;
-		Dictionary<String, int> difficulties = new Dictionary<string, int>();
-		PhysicsObject _; // väliaikainen
-		double scaling = 1.5;
-		PhysicsObject ship;
 
+		// Score
+		int score = 0;
+		Label scoreText;
+
+		// Health
+		int health = 100;
+		Label healthText;
+		Shape healthBar; // WIP or scrapped
+
+		// Images
+		Image shipImage = LoadImage("ship"); // Miksei esimerkeissä käytetä tiedostopäätettä?? https://github.com/Jypeli-JYU/Jypeli/blob/master/Examples/Koripallo/Koripallo.cs
+		Image enemyImage = LoadImage("enemy");
+
+		// Sounds
+		SoundEffect normal = LoadSoundEffect("shoot.wav");        // Jostain syystä huono äänenlaatu
+		SoundEffect slowed = LoadSoundEffect("shoot_slowed.wav"); //				^^^^^^
+
+		// Objects
+		PhysicsObject ship;
 		PlasmaCannon plasmaCannon;
-		private int health = 100;
-		private EnemyManager enemyManager = new EnemyManager();
+		PhysicsObject enemy;
 		PhysicsObject bullet;
+		List<PhysicsObject> enemies = new List<PhysicsObject>();
+		private int size;
+
+		// Logic 
+		RandomMoverBrain logic;
+		FollowerBrain approachLogic;
+
+		// Camera
+		private int x;
+		private int y;
+
+		// Difficulty
+		Dictionary<String, int> difficulties = new Dictionary<string, int>();
 		String difficulty = "medium";
 
+		// Misc
+		PhysicsObject _; // väliaikainen
+		Vector direction;
+		Random random = new Random();
+		Particle fire = new Particle();
+
+		// Game variables
+		private readonly int BASE_SPEED = 1250;
+		private int speed;
+		private readonly int BASE_ENEMY_SPEED=100;
+		private int enemySpeed;
+		private readonly int BASE_BULLET_SPEED=900;
+		private int bulletSpeed;
+		private readonly double BASE_ROTATION_SPEED=0.010;
+		private double rotationSpeed;
+		private readonly int BASE_FIRE_RATE=10;
+		private int fireRate;
+
+		// Leaderboard
+		Leaderboard leaderboard = new Leaderboard();
+		
+		String username = "test"; // Ask user later
 		public override void Begin()
 		{
-			difficulties.Add("easy", 50);
-			difficulties.Add("medium", 150);
-			difficulties.Add("hard", 300);
+			// Starting the server
+			var q = Task.Run(() =>
+			{leaderboard.StartServer();});
 
-			scoreText = new Label("test");
+			// Game var 
+			speed = BASE_SPEED;
+			enemySpeed = BASE_ENEMY_SPEED;
+			bulletSpeed = BASE_BULLET_SPEED;
+			rotationSpeed = BASE_ROTATION_SPEED;
+
+			// Sound
+			Game.MasterVolume = 0.5;
+
+			// Background
+			Level.Background.Color = new Color(25, 25, 25);
+			Level.Background.Image = (Image.CreateStarSky((int)Screen.Width, (int)Screen.Height, 45, transparent:true));
+
+			// Score
+			scoreText = new Label("0");
+			scoreText.TextColor = Color.White;
+			scoreText.Layer = Layer.CreateStaticLayer();
+			score = 0;
 			Add(scoreText);
 
+			// Health
+			health = 100;
 			healthText = new Label("" + health);
 			healthText.Position = new Vector(Convert.ToInt32((Screen.Size.X - Screen.Size.X * 1.5) * -1) - margin - fontSize, Convert.ToInt32((Screen.Size.Y - Screen.Size.Y * 1.5)) + margin + fontSize);
 			healthText.Color = Color.White;
 			healthText.TextColor = Color.Red;
-
-			
+			healthText.Font = new Font(fontSize);
 			Add(healthText);
 
-			Timer.CreateAndStart(1.5, AddEnemy);
-			Add(CreatePlayer(50,50));
+			// Difficulty
+			difficulties.Clear();
+			difficulties.Add("easy", 50);
+			difficulties.Add("medium", 150);
+			difficulties.Add("hard", 300);
 
-			Movement movement = new Movement();
-			movement.init(ship);
+			// Timer & Creating a player
+			ship = CreatePlayer(50, 80);
+			Add(ship);
+			Jypeli.Timer.CreateAndStart(3, AddEnemy);
+			Jypeli.Timer.CreateAndStart(15, IncreseDifficulty);
 
+			// Collision
 			AddCollisionHandler(ship, PlayerCollision);
 
-			plasmaCannon = new PlasmaCannon(10,10);
-			plasmaCannon.Angle = plasmaCannon.Angle + Angle.FromDegrees(90);
-			plasmaCannon.FireRate = 3;
+			// Misc
+			MessageDisplay.Font = new Font(12);
+			MessageDisplay.TextColor = Color.Blue;
+			MessageDisplay.Color = Color.White;
 
+			// Cannon
+			plasmaCannon = new PlasmaCannon(10,10);
+			plasmaCannon.Color = Color.Transparent;
+			plasmaCannon.AmmoIgnoresGravity = false;
+			plasmaCannon.Angle = plasmaCannon.Angle + Angle.FromDegrees(90);
+			plasmaCannon.FireRate = fireRate;
+			//plasmaCannon.AttackSound = normal;
 			ship.Add(plasmaCannon);
 
+			ResetSlowdown();
+
+			// Keybinds
 			PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
 			Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
-
-			Keyboard.Listen(Key.S, ButtonState.Down, movement.Down, "Down");
-			Keyboard.Listen(Key.W, ButtonState.Down, movement.Up, "Up");
-			Keyboard.Listen(Key.A, ButtonState.Down, movement.Left, "Left");
-			Keyboard.Listen(Key.D, ButtonState.Down, movement.Right, "Right");
+			Keyboard.Listen(Key.S, ButtonState.Down, Down, "Down");
+			Keyboard.Listen(Key.W, ButtonState.Down, Up, "Up");
+			Keyboard.Listen(Key.A, ButtonState.Down, Left, "Left");
+			Keyboard.Listen(Key.D, ButtonState.Down, Right, "Right");
 			Keyboard.Listen(Key.F, ButtonState.Down, Shoot, "Shoot");
+			Keyboard.Listen(Key.E, ButtonState.Down, Shoot, "Shoot");
+			Keyboard.Listen(Key.Space, ButtonState.Down, Boost, "Boost");
+			Keyboard.Listen(Key.Space, ButtonState.Released, ResetBoost, "ResetBoost");
+			Keyboard.Listen(Key.LeftShift, ButtonState.Down, Slowdown, "Slowdown");
+			Keyboard.Listen(Key.LeftShift, ButtonState.Released, ResetSlowdown, "ResetSlowdown");
 		}
 
-		public PhysicsObject CreatePlayer(int w, int h)
+		PhysicsObject CreatePlayer(int w, int h)
 		{
 			ship = new PhysicsObject(w, h);
 			ship.Shape = Shape.Triangle;
-			ship.Color = Color.Green;
+			ship.Image = shipImage;
 			ship.LinearDamping = 1.5;
 			ship.AngularDamping = 1.5;
-			return ship;
+			ship.CollisionIgnoreGroup = 2;
+			//ship.Add(PlayerLine());
+			//ship.IgnoresCollisionWith(shipAimLine);
+			ship.Mass = 2;
+			ship.Tag = "ship";
+			return ship;	
 		}
 
-		public static void EnemyCloseEvent()
+
+		PhysicsObject CreateEnemy(PhysicsObject ship)
+		{
+			size = random.Next(25, 60);
+			enemy = new PhysicsObject(size, size);
+			enemy.Shape = Shape.Rectangle;
+			enemy.Image = enemyImage;
+			enemy.X = random.Next(0, (int)800); // TODO: set as the screen width
+			enemy.Y = random.Next(0, (int)600); // TODO: set as the screen height
+			enemy.Brain = AddSmartLogic(ship);
+			enemy.CollisionIgnoreGroup = 1;
+			enemy.Tag = "Enemy";
+			enemy.RelativeAngle = Angle.FromDegrees(180);
+			enemy.CanRotate = true;
+			return enemy;
+		}
+
+		RandomMoverBrain AddDumbLogic()
+		{
+			logic = new RandomMoverBrain(200);
+			logic.ChangeMovementSeconds = 3;
+			return logic;
+		}
+
+		FollowerBrain AddSmartLogic(PhysicsObject target)
+		{
+			approachLogic = new FollowerBrain(target);
+			approachLogic.Speed = enemySpeed;
+			approachLogic.DistanceFar = 1200;
+			approachLogic.DistanceClose = 200;
+			approachLogic.FarBrain = AddDumbLogic();
+			approachLogic.TargetClose += EnemyCloseEvent;
+			approachLogic.TurnWhileMoving = true;
+			return approachLogic;
+		}
+
+		void EnemyCloseEvent()
 		{
 			
 		}
 
-		public void PlayerCollision(PhysicsObject collider, PhysicsObject target)
+		void PlayerCollision(PhysicsObject collider, PhysicsObject target)
 		{
-			health-=10;
-			if(health<=0)
+			health -= 10;
+			if (health <= 0)
 			{
-				collider.Destroy();
+				ship.Destroy();
+				Restart();
 			}
-			healthText.Text = ""+health;
+			healthText.Text = "" + health;
 		}
-		
-		public void BulletCollision(IPhysicsObject collider, IPhysicsObject target)
+		void BulletCollision(IPhysicsObject collider, IPhysicsObject target)
 		{
-			score++;
-			scoreText.Text = "" + score;
-			target.Destroy();
+			if(!target.IgnoresCollisionResponse)
+			{
+				score++;
+				scoreText.Text = "" + score;
+				target.Destroy();
+			}
 		}
-		public void Shoot()
+		void Shoot()
 		{
 			bullet = plasmaCannon.Shoot();
 			if (bullet != null)
 			{
+				bullet.MaximumLifetime = new TimeSpan(0,0,30);
+				bullet.MaxVelocity = bulletSpeed;
+				bullet.Color = Color.Gold;
+				bullet.Shape = Shape.Triangle;
+				bullet.Tag = "Bullet";
 				bullet.Collided += BulletCollision;
 			}
 		}
 
-		public static int GetDifficulty()
+		void Boost()
 		{
-			return 150;
+			MessageDisplay.Add("Boosting");
+			speed = BASE_SPEED*3;
+		}
+		void ResetBoost()
+		{
+			MessageDisplay.Add("!Boosting");
+
+			speed = BASE_SPEED;
+		}
+
+		void Slowdown()
+		{
+			MessageDisplay.Add("Slowdown");
+
+			speed = BASE_SPEED / 3;
+			enemySpeed = BASE_ENEMY_SPEED / 3;
+			bulletSpeed = BASE_BULLET_SPEED / 3;
+			rotationSpeed = BASE_ROTATION_SPEED / 3;
+			fireRate = BASE_FIRE_RATE / 3;
+			MessageDisplay.Add("" + enemies.Count);
+
+			if (approachLogic!=null)
+			{
+				approachLogic.Speed = enemySpeed;
+				foreach (var enemy_ in enemies)
+				{
+					enemy_.Brain = null;
+					enemy_.Brain = AddSmartLogic(ship);
+				}
+			}
+			if (plasmaCannon != null)	 
+			{
+				plasmaCannon.FireRate = fireRate;
+				plasmaCannon.AttackSound = slowed;
+			}
+		}
+
+		void ResetSlowdown()
+		{
+			MessageDisplay.Add("!Slowdown");
+
+			speed = BASE_SPEED;
+			enemySpeed = BASE_ENEMY_SPEED;
+			bulletSpeed = BASE_BULLET_SPEED;
+			rotationSpeed = BASE_ROTATION_SPEED;
+			MessageDisplay.Add("" + enemies.Count);
+			if(approachLogic!=null)
+			{
+				approachLogic.Speed = enemySpeed;
+				foreach (var enemy_ in enemies)
+				{
+					enemy_.Brain = AddSmartLogic(ship);
+				}
+			}
+
+			if (plasmaCannon != null)
+			{
+				plasmaCannon.FireRate = BASE_FIRE_RATE;
+				plasmaCannon.AttackSound = normal;
+			}
+		}
+
+		void IncreseDifficulty()
+		{
+			MessageDisplay.Add("Increasing Difficulty.");
+			enemySpeed += 25;
 		}
 
 		void AddEnemy()
 		{
-			Add(enemyManager.CreateEnemy(ship));
+			_ = CreateEnemy(ship);
+			enemies.Append(_);
+			Add(_);
 		}
 
+		void Down()
+		{
+			direction = Vector.FromLengthAndAngle(speed * -1, ship.Angle + Angle.FromDegrees(90));
+			ship.Push(direction);
+		}
+		void Up()
+		{
+			direction = Vector.FromLengthAndAngle(speed, ship.Angle + Angle.FromDegrees(90));
+			ship.Push(direction);
+		}
+		void Left()
+		{
+			ship.ApplyTorque(rotationSpeed);
+		}
+		void Right()
+		{
+			ship.ApplyTorque(rotationSpeed*-1);
+		}
+
+		 void Restart()
+		{
+			var q = Task.Run(() =>
+			{ leaderboard.SaveData(user:username, score:score) ; });
+			ResetLayers();
+			ClearTimers();
+			ClearLights();
+			ClearControls();
+			GC.Collect();
+			Camera.Reset();
+			ControlContext.Enable();
+			IsPaused = false;
+			Begin();
+		}
 	}
 }
